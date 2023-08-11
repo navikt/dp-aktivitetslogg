b39a7c42f41ae7e2f83bbeca48d73f059e032fb7
André Roaldseth <andre.roaldseth@nav.no>
2023-08-11 14:41:56 +0200

    Fiks test med minimum av required data

--- app/src/test/kotlin/no/nav/dagpenger/aktivitetslogg/mottak/AktivitetsloggMottakTest.kt
+++ app/src/test/kotlin/no/nav/dagpenger/aktivitetslogg/mottak/AktivitetsloggMottakTest.kt
@@ -2,11 +2,13 @@ package no.nav.dagpenger.aktivitetslogg.mottak
 
 import io.kotest.matchers.shouldBe
 import no.nav.dagpenger.aktivitetslogg.aktivitetslogg.PostgresAktivitetsloggRepository
+import no.nav.dagpenger.aktivitetslogg.api.models.AktivitetDTO
 import no.nav.dagpenger.aktivitetslogg.helpers.db.Postgres.withMigratedDb
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
+import java.util.UUID
 
 class AktivitetsloggMottakTest {
     private val repository by lazy { PostgresAktivitetsloggRepository(withMigratedDb()) }
@@ -23,11 +25,18 @@ class AktivitetsloggMottakTest {
 
     @Test
     fun `lagrer meldinger med aktivitetslogg`() {
-        val newMessage = JsonMessage.newMessage("aktivitetslogg", mapOf("ident" to "ident"))
+        val newMessage =
+            JsonMessage.newMessage(
+                "aktivitetslogg",
+                mapOf(
+                    "ident" to "ident",
+                    "hendelse" to mapOf("type" to "bar", "meldingsreferanseId" to UUID.randomUUID()),
+                    "aktiviteter" to emptyList<Object>(),
+                ),
+            )
         rapid.sendTestMessage(newMessage.toJson())
 
         with(repository.hentAktivitetslogg("ident").first()) {
-            println(this)
             this.atEventName shouldBe "aktivitetslogg"
             this.ident shouldBe "ident"
         }
