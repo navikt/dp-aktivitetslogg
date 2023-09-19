613d59b2f436419d1c0841d5ba467412fe616a0f
Geir André Lund <geir.andre.lund@nav.no>
2023-09-19 14:51:57 +0200

    Publisere aktivitetsloggen på felles format. Introduserer PersonHendelse som en felles interface for å få med sporing av ident og meldingreferanseId

--- aktivitetslogg/src/main/kotlin/no/nav/dagpenger/aktivitetslogg/IAktivitetsloggMediator.kt
+++ aktivitetslogg/src/main/kotlin/no/nav/dagpenger/aktivitetslogg/IAktivitetsloggMediator.kt
@@ -2,28 +2,27 @@ package no.nav.dagpenger.aktivitetslogg
 
 import no.nav.dagpenger.aktivitetslogg.serde.AktivitetsloggJsonBuilder
 
-
-interface AktivitetsloggMessageContext {
-    fun publish(eventNavn: String = "aktivitetslogg", message: Map<String, Any>)
-}
-
-interface AktivitetsloggHendelse : Subaktivitetskontekst {
+interface PersonHendelse : Aktivitetskontekst, IAktivitetslogg {
     fun ident(): String
     fun meldingsreferanseId(): String
 }
 
-
-interface IAktivitetsloggMediator : AktivitetsloggMessageContext {
-    fun publish(aktivitetsloggHendelse: AktivitetsloggHendelse) {
+interface IAktivitetsloggMediator {
+    fun håndter(personHendelse: PersonHendelse, publish: (AktivitetsLoggMelding) -> Unit) {
         publish(
-            message = mapOf(
-                "hendelse" to mapOf(
-                    "type" to aktivitetsloggHendelse.toSpesifikkKontekst().kontekstType,
-                    "meldingsreferanseId" to aktivitetsloggHendelse.meldingsreferanseId(),
-                ),
-                "ident" to aktivitetsloggHendelse.ident(),
-                "aktiviteter" to AktivitetsloggJsonBuilder(aktivitetsloggHendelse.aktivitetslogg).asList(),
+            AktivitetsLoggMelding(
+                mapOf(
+                    "hendelse" to mapOf(
+                        "type" to personHendelse.toSpesifikkKontekst().kontekstType,
+                        "meldingsreferanseId" to personHendelse.meldingsreferanseId(),
+                    ),
+                    "ident" to personHendelse.ident(),
+                    "aktiviteter" to AktivitetsloggJsonBuilder(personHendelse).asList(),
+                )
             )
         )
+    }
+    data class AktivitetsLoggMelding(val innhold: Map<String, Any>) {
+        val eventNavn: String = "aktivitetslogg"
     }
 }
