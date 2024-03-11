bb70aa4c4760742471a26cc7008aae39e9772c11
André Roaldseth <andre.roaldseth@nav.no>
2024-03-11 15:24:36 +0100

    Åpne SpesifikkKontekst slik at vi kan lage egne kontekster
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>

--- aktivitetslogg/src/main/kotlin/no/nav/dagpenger/aktivitetslogg/SpesifikkKontekst.kt
+++ aktivitetslogg/src/main/kotlin/no/nav/dagpenger/aktivitetslogg/SpesifikkKontekst.kt
@@ -1,6 +1,6 @@
 package no.nav.dagpenger.aktivitetslogg
 
-data class SpesifikkKontekst(val kontekstType: String, val kontekstMap: Map<String, String> = mapOf()) {
+open class SpesifikkKontekst(val kontekstType: String, open val kontekstMap: Map<String, String> = mapOf()) {
     fun melding() = kontekstType + kontekstMap.entries.joinToString(separator = "") { " ${it.key}: ${it.value}" }
 
     fun sammeType(other: Aktivitetskontekst) = this.kontekstType == other.toSpesifikkKontekst().kontekstType
