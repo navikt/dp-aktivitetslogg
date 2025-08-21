87aaf732feca3c13860fd6ec02bf01c2ae021f92
Giao The Cung <giaothe@gmail.com>
2025-08-21 15:17:52 +0200

    Ktlint

--- aktivitetslogg/src/main/kotlin/no/nav/dagpenger/aktivitetslogg/Varselkode.kt
+++ aktivitetslogg/src/main/kotlin/no/nav/dagpenger/aktivitetslogg/Varselkode.kt
@@ -5,6 +5,5 @@ import no.nav.dagpenger.aktivitetslogg.aktivitet.Varsel
 abstract class Varselkode(private val varseltekst: String) {
     internal fun varsel(kontekster: List<SpesifikkKontekst>): Varsel = Varsel.opprett(kontekster, this, varseltekst)
 
-
     override fun toString() = "${this::class.java.simpleName}: $varseltekst"
 }
