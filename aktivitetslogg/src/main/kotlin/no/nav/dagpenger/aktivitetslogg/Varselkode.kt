4989eada572012838fa80913e462175b586993f2
Giao The Cung <giaothe@gmail.com>
2025-08-21 15:13:08 +0200

    Fjerne død kode

--- aktivitetslogg/src/main/kotlin/no/nav/dagpenger/aktivitetslogg/Varselkode.kt
+++ aktivitetslogg/src/main/kotlin/no/nav/dagpenger/aktivitetslogg/Varselkode.kt
@@ -5,8 +5,6 @@ import no.nav.dagpenger.aktivitetslogg.aktivitet.Varsel
 abstract class Varselkode(private val varseltekst: String) {
     internal fun varsel(kontekster: List<SpesifikkKontekst>): Varsel = Varsel.opprett(kontekster, this, varseltekst)
 
-    /*internal fun funksjonellFeil(kontekster: List<SpesifikkKontekst>): Aktivitet.FunksjonellFeil =
-        Aktivitet.FunksjonellFeil.opprett(kontekster, this, funksjonellFeilTekst)*/
 
     override fun toString() = "${this::class.java.simpleName}: $varseltekst"
 }
