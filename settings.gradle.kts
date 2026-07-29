1dc51f6cb919630a0c73c5aef58c0e014d564e4f
André Roaldseth <andre.roaldseth@nav.no>
2026-07-29 09:54:25 +0200

    Gi dependabot mavenCentral()

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,7 @@ include("openapi")
 
 dependencyResolutionManagement {
     repositories {
+        mavenCentral()
         maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
     }
     versionCatalogs {
